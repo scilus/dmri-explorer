2e969fc35e6b08f86836beb7c397f60f7e76b77f
Julien Bernat <berj0505@dinf-0051-20b.DInf.fsci.usherbrooke.ca>
2022-03-16 15:23:00 -0400

    Add Vertices

--- Engine/src/texture.cpp
+++ Engine/src/texture.cpp
@@ -12,10 +12,14 @@ Texture::Texture(const std::shared_ptr<ApplicationState>& state,
 ,mIndicesBO(0)
 ,mIndirectBO(0)
 ,mIndirectCmd()
+,mAllVerticesData()
+,mTextureInfoData()
 {
     resetCS(std::shared_ptr<CoordinateSystem>(new CoordinateSystem(glm::mat4(1.0f), parent)));
     initializeModel();
     initializeMembers();
+    initializeGPUData();
+
 }
 
 Texture::~Texture()
@@ -52,6 +56,8 @@ void Texture::initializeMembers()
 
     const unsigned int dimX = image.dims().x;
     const unsigned int dimY = image.dims().y;
+
+    mAllVertices.resize(image.length());
     mIndices.resize(6);
     mIndirectCmd.resize(6);
     //First triangle
@@ -60,9 +66,17 @@ void Texture::initializeMembers()
         mIndices[i] = i;
     }
 
-    // const double r0 = image.at(0, 0, 0, 1)
-    // const double g0 = image.at(0, 0, 0, 2)
-    // const double b0 = image.at(0, 0, 0, 3)
+    const unsigned int nCoeffs = image.dims().w;
+    for(int i = 0; i < image.nbVox(); ++i)
+    {
+        glm::vec<3, uint> id3D = image.unravelIndex3d(i);
+
+        for(int k = 0; k < nCoeffs; ++k)
+        {
+            mAllVertices[i*nCoeffs + k] =
+                static_cast<float>(image.at(id3D.x, id3D.y, id3D.z, k));
+        }
+    }
 
     mIndirectCmd[0] = DrawArrays(0,0,0,255,255,255);
     mIndirectCmd[1] = DrawArrays(dimX,0,0,255,255,255);
@@ -78,6 +92,40 @@ void Texture::initializeMembers()
     mIndicesBO = genVBO<GLuint>(mIndices);
     mIndirectBO = genVBO<DrawArrays>(mIndirectCmd);
     timer.Stop();
+}
+
+void Texture::initializeGPUData()
+{
+    // temporary zero-filled array for all spheres vertices and normals
+    const auto image = mState->BackgroundImage.Get();
+    std::vector<glm::vec4> allVertices(image.nbVox());
+    std::vector<float> allRadiis(image.nbVox());
+
+    TextureData textureData;
+    textureData.NumVertices = image.nbVox();
+    textureData.NumIndices = 6;
+    textureData.NbCoeffs = image.dims().w;
+
+
+    GridData gridData;
+    gridData.SliceIndices = glm::ivec4(mState->VoxelGrid.SliceIndices.Get(), 0);
+    gridData.VolumeShape = glm::ivec4(mState->VoxelGrid.VolumeShape.Get(), 0);
+    gridData.CurrentSlice = 0;
+
+    mAllRadiisData = GPU::ShaderData(allRadiis.data(), GPU::Binding::allRadiis,
+                                     sizeof(float) * allRadiis.size());
+    mAllVerticesData = GPU::ShaderData(mAllVertices.data(), GPU::Binding::textureVertices,
+                                         sizeof(float) * mAllVertices.size());
+    mGridInfoData = GPU::ShaderData(&gridData, GPU::Binding::gridInfo,
+                                    sizeof(GridData));
+    mTextureInfoData = GPU::ShaderData(&textureData, GPU::Binding::sphereInfo,
+                                      sizeof(TextureData));
+
+    // push all data to GPU
+    mAllVerticesData.ToGPU();
+    mGridInfoData.ToGPU();
+    mAllRadiisData.ToGPU();
+    mTextureInfoData.ToGPU();
 }
 
 template <typename T>
